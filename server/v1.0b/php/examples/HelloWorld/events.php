60163f25d9bcb990a292484b4cc816bf75fdee5f
natedood <167116278+natedood@users.noreply.github.com>
2024-04-17 17:35:18 -0500

    removed code not needed

--- server/v1.0b/php/examples/HelloWorld/events.php
+++ server/v1.0b/php/examples/HelloWorld/events.php
@@ -8,16 +8,6 @@ $bind = new Bind();
 // example of handling a click event on a button with the id of myButton
 if( $req->type == 'click' && $req->id == 'getServertimeButton' ){
 
-    // get the form data
-    $formData = $req->getForm();
-
-    // get the content we will be outputting
-    ob_start();?>
-        <p>Button clicked! <?php echo date("Y-m-d H:i:s")?></p>
-        <p>Message: <?php echo $formData['firstname']?></p>
-    <?php
-    $output = ob_get_clean();
-
     // add the output to the response to bind client
     $res->addTask(array(
         'action'        => 'replace',
